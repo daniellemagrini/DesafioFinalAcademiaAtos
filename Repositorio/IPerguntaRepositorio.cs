0ff40e2e2b67cdb373097b591375582016a48140
Danielle Magrini <danielle.magrini@hotmail.com>
2022-07-16 14:49:35 -0300

    testando lógica das perguntas

--- Repositorio/IPerguntaRepositorio.cs
+++ Repositorio/IPerguntaRepositorio.cs
@@ -8,7 +8,11 @@ namespace DesafioFinalAcademiaAtos.Repositorio
         List<Pergunta> ListarPerguntasPortugues(string categoria);
         List<Pergunta> ListarPerguntasIngles(string categoria);
         List<Pergunta> ListarPerguntasLogica(string categoria);
-        
+        List<int> SelecionarPergunta();
+        List<int> SelecionarPerguntaPortugues();
+        List<int> SelecionarPerguntaIngles();
+        List<int> SelecionarPerguntaLogica();
+
 
     }
 }
--- Repositorio/PerguntaRepositorio.cs
+++ Repositorio/PerguntaRepositorio.cs
@@ -13,7 +13,61 @@ namespace DesafioFinalAcademiaAtos.Repositorio
 
         public List<Pergunta> ListarPerguntas()
         {
+            Pergunta pergunta = new Pergunta();
+            List<Pergunta> lista = new List<Pergunta>();
+            lista = _contexto.Pergunta.ToList();
+
+            if (pergunta.categoria == "Português")
+            {               
+                List<Pergunta> listaPortugues = new List<Pergunta>();
+
+                foreach (Pergunta itemPergunta in lista)
+                {
+                    listaPortugues.Add(itemPergunta);                   
+                }
+                return listaPortugues;
+            }
+
+            if (pergunta.categoria == "Inglês")
+            {
+                List<Pergunta> listaIngles = new List<Pergunta>();
+
+                foreach (Pergunta itemPergunta in lista)
+                {
+                    listaIngles.Add(itemPergunta);
+                }
+                return listaIngles;
+            }
+
+            if (pergunta.categoria == "Lógica")
+            {
+                List<Pergunta> listaLogica = new List<Pergunta>();
+
+                foreach (Pergunta itemPergunta in lista)
+                {
+                    listaLogica.Add(itemPergunta);
+                }
+                return listaLogica;
+            }
+
             return _contexto.Pergunta.ToList();
+        }
+
+        public List<int> SelecionarPergunta()
+        {
+            List<Pergunta> lista = new List<Pergunta>();
+            lista = ListarPerguntas();
+            List<int> listaSelecao = new List<int>();
+            List<int> listaSelecionados = new List<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int idSelecao = r.Next(1, lista.Count);
+                listaSelecionados.Add(idSelecao);
+                listaSelecao.Add(idSelecao);
+            }
+            return listaSelecao;
         }
 
         public List<Pergunta> ListarPerguntasIngles(string categoria)
@@ -65,6 +119,55 @@ namespace DesafioFinalAcademiaAtos.Repositorio
                 }
             }
             return listaPortugues;
+        }
+
+        public List<int> SelecionarPerguntaIngles()
+        {
+            string categoria = "Inglês";
+            List<Pergunta> lista = new List<Pergunta>();
+            lista = ListarPerguntasIngles(categoria);
+            List<int> listaSelecao = new List<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int idSelecao = r.Next(1, lista.Count);
+                listaSelecao.Add(idSelecao);
+            }
+            return listaSelecao;
+        }
+
+        public List<int> SelecionarPerguntaLogica()
+        {
+            string categoria = "Lógica";
+            List<Pergunta> lista = new List<Pergunta>();
+            lista = ListarPerguntasIngles(categoria);
+            List<int> listaSelecao = new List<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int idSelecao = r.Next(1, lista.Count);
+                listaSelecao.Add(idSelecao);
+            }
+            return listaSelecao;
+        }
+
+        public List<int> SelecionarPerguntaPortugues()
+        {
+            string categoria = "Português";
+            List<Pergunta> lista = new List<Pergunta>();
+            lista = ListarPerguntasIngles(categoria);
+            List<int> listaSelecao = new List<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                int idSelecao = r.Next(1, lista.Count);
+                listaSelecao.Add(idSelecao);
+            }
+            return listaSelecao;
         }
     }
 }
